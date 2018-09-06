296b0baa808d65a076b9a1ddd4f9554b65ad638f
Marc Mills <mdmills1990@gmail.com>
2018-09-06 19:41:29 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+
+def match(words)
+  alph_word = @word.split("").sort.join("")
+    words.find do |array_word|
+    if alph_word == array_word.split("").sort.join("")
+      puts array_word
+    else
+      []
+    end
+  end
+end
+
+
+end
