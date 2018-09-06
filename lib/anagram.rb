7401f3dabdfd47cb8aa825295776bc7d8cae9003
Marc Mills <mdmills1990@gmail.com>
2018-09-06 19:50:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+
+def match(words)
+  alph_word = @word.split("").sort.join("")
+    words.find_all do |array_word|
+    if alph_word == array_word.split("").sort.join("")
+    array_word
+    end
+  end
+end
+
+
+end
